0a2778c702970300f32e5771661e082db2d07553
kovgreen <kovgreen@gmail.com>
2020-05-26 21:38:04 +0300

    Updates

---
+++ precache-manifest.c9319a5a8cab834af3ccfcece6d6fbb8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "094a45e0420a015d8d59c78765c79611",
+    "url": "/goit-react-hw-01-components/index.html"
+  },
+  {
+    "revision": "2aa7f3d5f78536e53900",
+    "url": "/goit-react-hw-01-components/static/css/main.b21e03e9.chunk.css"
+  },
+  {
+    "revision": "86652f30c9cdeb257c28",
+    "url": "/goit-react-hw-01-components/static/js/2.262443ce.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/goit-react-hw-01-components/static/js/2.262443ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2aa7f3d5f78536e53900",
+    "url": "/goit-react-hw-01-components/static/js/main.f2a07792.chunk.js"
+  },
+  {
+    "revision": "e6e52a6c8a65f3c70b2a",
+    "url": "/goit-react-hw-01-components/static/js/runtime-main.69b22fd4.js"
+  }
+]);
